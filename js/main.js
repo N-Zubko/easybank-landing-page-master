3e2e5b78aeb76b2ab76e422269e7e2a0d326bc9c
Nadia Zubko <ne.nadezda@gmail.com>
2025-03-04 01:20:34 -0700

    Add navigation class to ul and add backdrop. Add logic for the navigation and backdrop on click

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    this.classList.toggle('active');
-  });
+  this.classList.toggle('active');
+  document.querySelector('.navigation').classList.toggle('active');
+  document.querySelector('.backdrop').classList.toggle('active');
+});
   
