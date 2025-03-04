bc37bf4b5a235f2c0ee6055ac6dd2e5ec5e08cb5
Nadia Zubko <ne.nadezda@gmail.com>
2025-03-04 01:30:24 -0700

    Add overflow hidden toggle to the hamburger menu

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
   this.classList.toggle('active');
   document.querySelector('.navigation').classList.toggle('active');
   document.querySelector('.backdrop').classList.toggle('active');
+  if (this.classList.contains('active')) {
+    document.querySelector('body').setAttribute('style', 'overflow: hidden');
+  } else {
+    document.querySelector('body').removeAttribute('style');
+  }
 });
   
